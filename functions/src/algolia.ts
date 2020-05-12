28f32d2fafedc9beff919f063422fd9820b1710f
ReiMeroy <xvwlllwvx@gmail.com>
2020-05-12 22:51:41 +0900

    feat(function):addRecord

---
+++ functions/src/algolia.ts
@@ -0,0 +1,29 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+const algoliasearch = require('algoliasearch');
+
+const ALGOLIA_ID = functions.config().algolia.app_id;
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+const index = client.initIndex('articles');
+
+export const addRecord = functions
+  .region('asia-northeast1')
+  .firestore.document('articles/{id}')
+  .onCreate((snap, context) => {
+    const data = snap.data() as {
+      id: string;
+      title: string;
+      body: string;
+      public: boolean;
+      createdAt: admin.firestore.Timestamp;
+    };
+    const value = {
+      ...data,
+      objectID: data.id,
+      createdAt: data.createdAt.toMillis(),
+    };
+    return index.saveObject(value);
+  });
+
+export const console = functions.firestore.document('');
