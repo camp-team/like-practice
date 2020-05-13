c4b2937047f92beec9769e71401e412c53755f09
ReiMeroy <xvwlllwvx@gmail.com>
2020-05-13 17:18:05 +0900

    fix(articleService):コレクションのパスミスを修正

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -3,6 +3,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable, of } from 'rxjs';
 import { Article } from '../interfaces/article';
 import { firestore } from 'firebase';
+import { ArticleListComponent } from '../article-list/article-list/article-list.component';
 
 @Injectable({
   providedIn: 'root',
@@ -10,17 +11,20 @@ import { firestore } from 'firebase';
 export class ArtileService {
   constructor(private db: AngularFirestore) {}
 
-  getArticle(id: String): Observable<Article> {
-    return this.db.doc<Article>(`'articles/${id}'`).valueChanges();
+  getArticleByID(id: String): Observable<Article> {
+    return this.db.doc<Article>(`articles/${id}`).valueChanges();
+  }
+  getArticles(): Observable<Article[]> {
+    return this.db.collection<Article>('articles').valueChanges();
   }
 
   updateArticle(article: Article): Promise<void> {
-    return this.db.doc<Article>(`'articles/${article.id}'`).update(article);
+    return this.db.doc<Article>(`articles/${article.id}`).update(article);
   }
 
   createArticle(article: Omit<Article, 'id' | 'createdAt'>): Promise<void> {
     const id = this.db.createId();
-    return this.db.doc<Article>(`'articles/${id}'`).set({
+    return this.db.doc<Article>(`articles/${id}`).set({
       id,
       createdAt: firestore.Timestamp.now(),
       ...article,
@@ -28,6 +32,6 @@ export class ArtileService {
   }
 
   deleteArticle(id: String): Promise<void> {
-    return this.db.doc<Article>(`'articles/${id}'`).delete();
+    return this.db.doc<Article>(`articles/${id}`).delete();
   }
 }
