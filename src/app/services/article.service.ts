4a3757c68865d1367c96849c5c02ccdd458b1f4f
ReiMeroy <xvwlllwvx@gmail.com>
2020-05-15 00:30:20 +0900

    fix(articleService):getLikedArticleのObservableをやめる

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable, of } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Article } from '../interfaces/article';
 import { firestore } from 'firebase';
-import { ArticleListComponent } from '../article-list/article-list/article-list.component';
 
 @Injectable({
   providedIn: 'root',
@@ -11,9 +12,24 @@ import { ArticleListComponent } from '../article-list/article-list/article-list.
 export class ArtileService {
   constructor(private db: AngularFirestore) {}
 
+  getLikedArticleIds(uid: string): Promise<string[]> {
+    return this.db
+      .collectionGroup<{
+        uid: string;
+        articleId: string;
+      }>('likeUsers', (ref) => ref.where('uid', '==', uid))
+      .valueChanges()
+      .pipe(
+        map((res) => res.map((item) => item.articleId)),
+        take(1)
+      )
+      .toPromise();
+  }
+
   getArticleByID(id: String): Observable<Article> {
     return this.db.doc<Article>(`articles/${id}`).valueChanges();
   }
+
   getArticles(): Observable<Article[]> {
     return this.db.collection<Article>('articles').valueChanges();
   }
@@ -27,11 +43,23 @@ export class ArtileService {
     return this.db.doc<Article>(`articles/${id}`).set({
       id,
       createdAt: firestore.Timestamp.now(),
+      likeCount: 0,
       ...article,
     });
   }
 
   deleteArticle(id: String): Promise<void> {
     return this.db.doc<Article>(`articles/${id}`).delete();
+  }
+
+  likeArticle(articleId: string, uid: string): Promise<void> {
+    return this.db.doc(`articles/${articleId}/likeUsers/${uid}`).set({
+      uid,
+      articleId,
+    });
+  }
+
+  unLikeArticle(articleId: string, uid: string): Promise<void> {
+    return this.db.doc(`articles/${articleId}/likeUsers/${uid}`).delete();
   }
 }
